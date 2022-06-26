0ee40200c5269dc4a66a458a26ca83e61ef3235f
Dien Cap <baodien1405@gmail.com>
2022-06-26 23:54:16 +0700

    Using FlatList to render items and add Swipeable lib

--- App.js
+++ App.js
@@ -3,10 +3,16 @@ import { View } from "react-native";
 import AppButton from "./app/components/AppButton";
 import AppText from "./app/components/AppText";
 import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/components/ListingDetailsScreen";
+import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
+import Screen from "./app/components/Screen";
+import AccountScreen from "./app/screen/AccountScreen";
+import ListingDetailsScreen from "./app/screen/ListingDetailsScreen";
+import ListingsScreen from "./app/screen/ListingsScreen";
+import MessagesScreen from "./app/screen/MessagesScreen";
 import ViewImageScreen from "./app/screen/ViewImageScreen";
 import WelcomeScreen from "./app/screen/WelcomeScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <ListingsScreen />;
 }
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,29 +1,52 @@
 import React from "react";
-import { Image, View, StyleSheet } from "react-native";
+import { Image, View, StyleSheet, TouchableHighlight } from "react-native";
 import colors from "../config/colors";
 import AppText from "./AppText";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-function ListItem({ image, title, subTitle }) {
+function ListItem({
+  image,
+  title,
+  subTitle,
+  IconComponent,
+  renderRightActions,
+  onPress,
+}) {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={image} />
-      <View>
-        <AppText style={styles.title}>{title}</AppText>
-        <AppText style={styles.subTitle}>{subTitle}</AppText>
-      </View>
-    </View>
+    <GestureHandlerRootView>
+      <Swipeable renderRightActions={renderRightActions}>
+        <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+          <View style={styles.container}>
+            {IconComponent}
+            {image && <Image style={styles.image} source={image} />}
+            <View style={styles.detailsContainer}>
+              <AppText style={styles.title}>{title}</AppText>
+              {subTitle && (
+                <AppText style={styles.subTitle}>{subTitle}</AppText>
+              )}
+            </View>
+          </View>
+        </TouchableHighlight>
+      </Swipeable>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
+    padding: 15,
+    backgroundColor: colors.white,
   },
   image: {
     width: 70,
     height: 70,
     borderRadius: 35,
-    marginRight: 10,
+  },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
   },
   title: {
     fontWeight: "500",
